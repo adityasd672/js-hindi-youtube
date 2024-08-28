0a2e76d2fbb9ed67fcdb35aaafbdc51a26d560c0
adityasd672 <adityapts999@gmail.com>
2024-08-28 07:22:21 +0000

    to be continue about string...

---
+++ 01_basics/05_string.js
@@ -0,0 +1,10 @@
+const name = "aditya"
+const repCount = 50
+
+// console.log(name + repCount + " vValue")
+
+console.log(`Hello my name is ${name} and my repo count is ${repCount}`)
+
+const gameName = new String('hiteshChoudhary')
+console.log(gameName)
+console.log(gameName.__proto__);

4551fe0b85e1987a025d027235bf455dec10a17a
adityasd672 <adityapts999@gmail.com>
2024-08-29 07:12:52 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -3,8 +3,32 @@ const repCount = 50
 
 // console.log(name + repCount + " vValue")
 
-console.log(`Hello my name is ${name} and my repo count is ${repCount}`)
+// console.log(`Hello my name is ${name} and my repo count is ${repCount}`)
 
 const gameName = new String('hiteshChoudhary')
-console.log(gameName)
-console.log(gameName.__proto__);
+// console.log(gameName)
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4) // substr => can't give -ve
+console.log(newString)
+
+const anotherString = gameName.slice(-15,4);
+console.log(anotherString)
+
+const newStringOne = "       hitesh        "
+console.log(newStringOne)
+console.log(newStringOne.trim()) // works only on whitespaces characters or newline characters
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('hitesh'))
+
+console.log(gameName.split('-'))

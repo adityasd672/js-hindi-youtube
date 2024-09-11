07b38797cbc5f48ef10b49558d44d13d9146bdea
adityasd672 <adityapts999@gmail.com>
2024-09-11 19:00:05 +0000

    high order array loops

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,19 @@
+
+let index = 0
+while(index <= 10){
+    // console.log(`Value of index is ${index++}`);
+    index++;    
+}
+
+let myArray = ['flash', 'batman', 'superman']
+
+let arr = 0
+
+// while (arr< myArray.length) {
+//     console.log(`Value is ${myArray[arr++]}`);    
+// }
+
+do {
+    console.log(`Value is ${myArray[arr++]}`);
+    
+} while (arr<0);
